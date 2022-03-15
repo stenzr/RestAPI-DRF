aaa248f6b8bcb801f0832d0b6434ebabc686c9a5
stenzr <rkrohitkr01@gmail.com>
2022-03-15 17:34:46 +0530

    [FIX] version upgrade issues with url > re_path

--- restApi/restApi/urls.py
+++ restApi/restApi/urls.py
@@ -14,11 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url, include 
+from django.urls import path, include, re_path as url
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    url(r'^', include('tutorials.urls'))
-]
 
+urlpatterns = [path("admin/", admin.site.urls), url(r"^", include("tutorials.urls"))]
--- restApi/tutorials/urls.py
+++ restApi/tutorials/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import path, re_path as url
 from tutorials import views
 
 urlpatterns = [
